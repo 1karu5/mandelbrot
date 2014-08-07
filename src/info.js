f26f43bfab16a72e8fdf55ebfae56a83d118773f
Atlantis89 <Atlantis89@users.noreply.github.com>
2014-08-07 19:28:28 +0200

    so mit event und so

--- src/info.js
+++ src/info.js
@@ -1,33 +1,42 @@
-(function(){
-	var top = 0;
-	var left = 0;
-r.define("info", {
-	inherit : "rulus.div",
+(function() {
+    r.define("info", {
+        inherit : "rulus.div",
 
-	initClass : function() {
-		this.css({
-			position : "absolute",
-			top : top,
-			left : left,
-			"background-color" : "red",
-			width : 200,
-			height : 100
-		});
-	},
-	init : function(){
-		var pressed = false;
-		this.bind({
-			mousedown : function(){
-				pressed = true;
-			},
-			mouseup : function(){
-				pressed = false;
-			},
-			mousemove : function(e){
-				console.log(e);
-			}
-		});
-	}
-});
+        initClass : function() {
+            this.css({
+                position : "absolute",
+                top : 0,
+                left : 0,
+                "background-color" : "red",
+                width : 200,
+                height : 100
+            });
+        },
+        init : function() {
+            var pressed = false;
+            var xBeimKlick;
+            var yBeimKlick;
+            this.bind({
+                mousedown : function(e) {
+                    pressed = true;
+                    xBeimKlick = e.clientX;
+                    yBeimKlick = e.clientY;
+                    console.log(xBeimKlick);
+                    console.log(yBeimKlick);
+                },
+                mouseup : function(e) {
+                    pressed = false;
+                },
+                mousemove : function(e) {
+                    if (pressed) {
+                        this.style({
+                            "top" : e.clientY,
+                            "left" : e.clientX
+                        });
+                    }
+                }
+            });
+        }
+    });
 
 })();
