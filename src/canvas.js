1d3798cb293d10b4d29f3d5f670bf7aa8f0c3f71
1karu5 <1karu5@users.noreply.github.com>
2014-08-07 17:58:05 +0200

    canvas erweitert und verbessert

--- src/canvas.js
+++ src/canvas.js
@@ -1,16 +1,38 @@
 r.define("canvas", {
-	inherit : "rulus.ui",
-	tag : "canvas",
-
-	initClass : function() {
-		this.css({
-			position : "absolute",
-			top : 0,
-			left : 0
-		});
-	},
-
-	getContext2d : function() {
-		return this.obj.getContext("2d");
-	}
+    inherit: "rulus.ui",
+    tag: "canvas",
+    
+    initClass: function(){
+        this.css({
+            position: "absolute",
+            top: 0,
+            left: 0
+        });
+    },
+    
+    width: function(w){
+        if (w == undefined) {
+            return this.attr("width");
+        }
+        this.attr("width", w);
+    },
+    
+    height: function(h){
+        if (h == undefined) {
+            return this.attr("height");
+        }
+        this.attr("height", h);
+    },
+    
+    //clear canvas
+    clear: function(){
+        this.getContext2d().clearRect(0, 0, this.width(), this.height());
+    },
+    //return context
+    getContext2d: function(){
+        if (!this.settings.context2d) {
+            this.settings.context2d = this.obj.getContext("2d");
+        }
+        return this.settings.context2d;
+    }
 });
